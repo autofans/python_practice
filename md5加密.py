e7799005251ddc6343757fe20b215912a88f3213
autofans <438117814@qq.com>
2020-12-18 00:22:16 +0800

    222

--- md5加密.py
+++ md5加密.py
@@ -6,6 +6,9 @@
 import hashlib
 
 
+USER_LIST = []    # 存用户名和密码
+
+
 def get_md5(data):
 
     obj = hashlib.md5("ihgidkjgkdhdieidn:jidje:kiFDKiheEkid%%@ki%k".encode("utf8"))
@@ -17,7 +20,34 @@ def get_md5(data):
     return result
 
 
-v = get_md5("234555")
+def register():
+    print("**********用户注册**********")
 
-print(v)
+    while True:
+        user = input("请输入用户名：")
+        if user == "N":
+            return
+
+        pwd = input("请输入密码：")
+        temp = {"username": user, "password": get_md5(pwd)}
+        USER_LIST.append(temp)
+
+
+def login():
+    print("**********用户登录**********")
+    user = input("请输入用户名：")
+    pwd = input("请输入密码：")
+
+    for item in USER_LIST:
+        if item["username"] == user and item["password"] == get_md5(pwd):
+            return True
+
+
+register()
+result = login()
+
+if result:
+    print("登录成功！")
+else:
+    print("登录失败！！！")
 
